fc94d4d93754ab4e247cf78d123a8e43d895a3e4
jordanrowe <37838443+jordanrowe@users.noreply.github.com>
2023-01-25 16:20:06 +0000

    BDOG-2387 remove latest flag for deleted/archived services

--- app/uk/gov/hmrc/serviceconfigs/service/SlugInfoService.scala
+++ app/uk/gov/hmrc/serviceconfigs/service/SlugInfoService.scala
@@ -18,26 +18,39 @@ package uk.gov.hmrc.serviceconfigs.service
 
 import cats.instances.all._
 import cats.syntax.all._
+import play.api.Logger
+
 import javax.inject.{Inject, Singleton}
 import uk.gov.hmrc.http.HeaderCarrier
-import uk.gov.hmrc.serviceconfigs.connector.ReleasesApiConnector
+import uk.gov.hmrc.serviceconfigs.connector.{GithubRawConnector, ReleasesApiConnector, TeamsAndRepositoriesConnector}
 import uk.gov.hmrc.serviceconfigs.model._
-import uk.gov.hmrc.serviceconfigs.persistence.SlugInfoRepository
+import uk.gov.hmrc.serviceconfigs.persistence.{SlugInfoRepository, SlugVersionRepository}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class SlugInfoService @Inject()(
-  slugInfoRepository   : SlugInfoRepository
-, releasesApiConnector : ReleasesApiConnector
+  slugInfoRepository     : SlugInfoRepository
+, slugVersionRepository  : SlugVersionRepository
+, releasesApiConnector   : ReleasesApiConnector
+, teamsAndReposConnector : TeamsAndRepositoriesConnector
+, githubRawConnector     : GithubRawConnector
 )(implicit
   ec: ExecutionContext
 ) {
+
+  private val logger = Logger(getClass)
 
   def updateMetadata()(implicit hc: HeaderCarrier): Future[Unit] =
     for {
       serviceNames           <- slugInfoRepository.getUniqueSlugNames
       serviceDeploymentInfos <- releasesApiConnector.getWhatIsRunningWhere
+      activeRepos            <- teamsAndReposConnector.getRepos(archived = Some(false))
+                                  .map(_.map(_.name))
+      decommissionedServices <- githubRawConnector.decommissionedServices
+      latestServices         <- slugInfoRepository.getAllLatestSlugInfos
+                                  .map(_.map(_.name))
+      inactiveServices       =  latestServices.diff(activeRepos)
       allServiceDeployments  =  serviceNames.map { serviceName =>
                                   val deployments       = serviceDeploymentInfos.find(_.serviceName == serviceName).map(_.deployments)
                                   val deploymentsByFlag = Environment.values
@@ -57,5 +70,23 @@ class SlugInfoService @Inject()(
                                     case (_, (flag, Some(version))) => slugInfoRepository.setFlag(flag, serviceName, version)
                                   }
                                 }
+      _                      <- slugInfoRepository.clearFlags(SlugInfoFlag.values, decommissionedServices)
+      _                      <- if (inactiveServices.nonEmpty) {
+                                  logger.info(s"Removing latest flag from the following inactive services: ${inactiveServices.mkString(", ")}")
+                                  slugInfoRepository.clearFlags(List(SlugInfoFlag.Latest), inactiveServices.toList)
+                                } else Future.unit
+      missingLatestFlag      =  serviceNames.intersect(activeRepos).diff(decommissionedServices).diff(latestServices)
+      _                      <- if (missingLatestFlag.nonEmpty) {
+                                  logger.warn(s"The following services are missing Latest flag - setting latest flag based on latest version: ${missingLatestFlag.mkString(",")}")
+                                  missingLatestFlag.foldLeftM(()) { (_, serviceName) =>
+                                    for {
+                                      optVersion <- slugVersionRepository.getMaxVersion(serviceName)
+                                      _          <- optVersion match {
+                                                      case Some(version) => slugInfoRepository.setFlag(SlugInfoFlag.Latest, serviceName, version)
+                                                      case None          => logger.warn(s"No max version found for $serviceName"); Future.unit
+                                                    }
+                                    } yield ()
+                                  }
+                                } else Future.unit
     } yield ()
 }
---
+++ test/uk/gov/hmrc/serviceconfigs/service/SlugInfoServiceSpec.scala
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.serviceconfigs.service
+
+import org.mockito.scalatest.MockitoSugar
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.serviceconfigs.connector.TeamsAndRepositoriesConnector.Repo
+import uk.gov.hmrc.serviceconfigs.connector.{GithubRawConnector, ReleasesApiConnector, TeamsAndRepositoriesConnector}
+import uk.gov.hmrc.serviceconfigs.model.{SlugInfo, SlugInfoFlag, Version}
+import uk.gov.hmrc.serviceconfigs.persistence.{SlugInfoRepository, SlugVersionRepository}
+
+import java.time.Instant
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class SlugInfoServiceSpec
+  extends AnyWordSpec
+    with Matchers
+    with ScalaFutures
+    with MockitoSugar
+    with IntegrationPatience {
+
+  private implicit val hc: HeaderCarrier = HeaderCarrier()
+
+  "SlugInfoService.updateMetadata" should {
+    "clear flags of decommissioned services" in {
+      val boot = Boot.init
+
+      val decommissionedServices = List("service1")
+
+      when(boot.mockedSlugInfoRepository.getUniqueSlugNames)
+        .thenReturn(Future.successful(Seq.empty))
+
+      when(boot.mockedReleasesApiConnector.getWhatIsRunningWhere)
+        .thenReturn(Future.successful(List.empty))
+
+      when(boot.mockedTeamsAndReposConnector.getRepos(eqTo(Some(false)), any[Option[String]]))
+        .thenReturn(Future.successful(Seq.empty))
+
+      when(boot.mockedGithubRawConnector.decommissionedServices)
+        .thenReturn(Future.successful(decommissionedServices))
+
+      when(boot.mockedSlugInfoRepository.getAllLatestSlugInfos)
+        .thenReturn(Future.successful(Seq.empty))
+
+      when(boot.mockedSlugInfoRepository.clearFlags(any[List[SlugInfoFlag]], any[List[String]]))
+        .thenReturn(Future.unit)
+
+      boot.service.updateMetadata().futureValue
+
+      verify(boot.mockedSlugInfoRepository).clearFlags(SlugInfoFlag.values, decommissionedServices)
+    }
+
+    "clear latest flag for services that have been deleted/archived" in {
+      val boot = Boot.init
+
+      val knownServices  = List("service1", "service2", "service3")
+      val activeServices = List("service1", "service3").map(Repo.apply)
+      val archived       = List("service2")
+
+      when(boot.mockedSlugInfoRepository.getUniqueSlugNames)
+        .thenReturn(Future.successful(knownServices))
+
+      when(boot.mockedReleasesApiConnector.getWhatIsRunningWhere)
+        .thenReturn(Future.successful(List.empty))
+
+      when(boot.mockedTeamsAndReposConnector.getRepos(eqTo(Some(false)), any[Option[String]]))
+        .thenReturn(Future.successful(activeServices))
+
+      when(boot.mockedGithubRawConnector.decommissionedServices)
+        .thenReturn(Future.successful(List.empty))
+
+      when(boot.mockedSlugInfoRepository.getAllLatestSlugInfos)
+        .thenReturn(Future.successful(knownServices.map(Boot.toSlugInfo)))
+
+      when(boot.mockedSlugInfoRepository.clearFlag(any[SlugInfoFlag], any[String]))
+        .thenReturn(Future.unit)
+
+      when(boot.mockedSlugInfoRepository.clearFlags(any[List[SlugInfoFlag]], any[List[String]]))
+        .thenReturn(Future.unit)
+
+      boot.service.updateMetadata().futureValue
+
+      verify(boot.mockedSlugInfoRepository).clearFlags(List(SlugInfoFlag.Latest), archived)
+    }
+
+    "detect any services that do not have a 'latest' flag and set based on maxVersion" in {
+      val boot = Boot.init
+
+      val knownServices  = List("service1", "service2", "service3")
+      val activeServices = List("service1", "service2", "service3").map(Repo.apply)
+      val latestServices = List("service1", "service3")
+      val missingLatest  = "service2"
+      val maxVersion     = Version("1.0.0")
+
+      when(boot.mockedSlugInfoRepository.getUniqueSlugNames)
+        .thenReturn(Future.successful(knownServices))
+
+      when(boot.mockedReleasesApiConnector.getWhatIsRunningWhere)
+        .thenReturn(Future.successful(List.empty))
+
+      when(boot.mockedTeamsAndReposConnector.getRepos(eqTo(Some(false)), any[Option[String]]))
+        .thenReturn(Future.successful(activeServices))
+
+      when(boot.mockedGithubRawConnector.decommissionedServices)
+        .thenReturn(Future.successful(List.empty))
+
+      when(boot.mockedSlugInfoRepository.getAllLatestSlugInfos)
+        .thenReturn(Future.successful(latestServices.map(Boot.toSlugInfo)))
+
+      when(boot.mockedSlugInfoRepository.clearFlag(any[SlugInfoFlag], any[String]))
+        .thenReturn(Future.unit)
+
+      when(boot.mockedSlugInfoRepository.clearFlags(any[List[SlugInfoFlag]], any[List[String]]))
+        .thenReturn(Future.unit)
+
+      when(boot.mockedSlugVersionRepository.getMaxVersion(any[String]))
+        .thenReturn(Future.successful(Some(maxVersion)))
+
+      when(boot.mockedSlugInfoRepository.setFlag(any[SlugInfoFlag], any[String], any[Version]))
+        .thenReturn(Future.unit)
+
+      boot.service.updateMetadata().futureValue
+
+      verify(boot.mockedSlugInfoRepository).setFlag(SlugInfoFlag.Latest, missingLatest, maxVersion)
+    }
+  }
+  
+  case class Boot(
+    mockedSlugInfoRepository     : SlugInfoRepository
+  , mockedSlugVersionRepository  : SlugVersionRepository
+  , mockedReleasesApiConnector   : ReleasesApiConnector
+  , mockedTeamsAndReposConnector : TeamsAndRepositoriesConnector
+  , mockedGithubRawConnector     : GithubRawConnector
+  , service                      : SlugInfoService
+  )
+  
+  object Boot {
+    def init: Boot = {
+      val mockedSlugInfoRepository     = mock[SlugInfoRepository]
+      val mockedSlugVersionRepository  = mock[SlugVersionRepository]
+      val mockedReleasesApiConnector   = mock[ReleasesApiConnector]
+      val mockedTeamsAndReposConnector = mock[TeamsAndRepositoriesConnector]
+      val mockedGithubRawConnector     = mock[GithubRawConnector]
+      
+      val service = new SlugInfoService(
+                          mockedSlugInfoRepository
+                        , mockedSlugVersionRepository
+                        , mockedReleasesApiConnector
+                        , mockedTeamsAndReposConnector
+                        , mockedGithubRawConnector
+                        )
+
+      Boot(
+        mockedSlugInfoRepository
+      , mockedSlugVersionRepository
+      , mockedReleasesApiConnector
+      , mockedTeamsAndReposConnector
+      , mockedGithubRawConnector
+      , service
+      )
+    }
+
+    def toSlugInfo(name: String): SlugInfo =
+      SlugInfo(
+        uri = ""
+        , created = Instant.now
+        , name = name
+        , version = Version("0.0.0")
+        , classpath = ""
+        , dependencies = List.empty
+        , applicationConfig = ""
+        , loggerConfig = ""
+        , slugConfig = ""
+      )
+  }
+
+}
