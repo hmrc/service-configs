be577f928def083a34d29c2faf4184bd3f86b0bd
peteanning <22272449+peteanning@users.noreply.github.com>
2023-07-19 15:46:13 +0100

    BDOG-2659: formatting

--- app/uk/gov/hmrc/serviceconfigs/controller/InternalAuthConfigController.scala
+++ app/uk/gov/hmrc/serviceconfigs/controller/InternalAuthConfigController.scala
@@ -30,22 +30,17 @@ import scala.concurrent.ExecutionContext
 
 @Singleton
 class InternalAuthConfigController @Inject()(
-  internalAuthConfigRepository: InternalAuthConfigRepository,
-  mcc          : MessagesControllerComponents
+  internalAuthConfigRepository : InternalAuthConfigRepository,
+  mcc                          : MessagesControllerComponents
 )(implicit
   ec: ExecutionContext
 ) extends BackendController(mcc) with Logging {
 
-  implicit val format = uk.gov.hmrc.serviceconfigs.model.InternalAuthConfig.format
+  private implicit val format = uk.gov.hmrc.serviceconfigs.model.InternalAuthConfig.format
 
-  @ApiOperation(
-    value = "Retrieves all of the config for a given service, broken down by environment",
-    notes = """Searches all config sources for all environments and pulls out the the value of each config key"""
-  )
-  def serviceConfig(
-    @ApiParam(value = "The service name to query") serviceName: ServiceName
+  def internalAuthConfig(
+     serviceName: ServiceName
   ): Action[AnyContent] = Action.async { implicit request =>
-    logger.info(s"Called ${request.uri} with ${serviceName.asString} ")
     internalAuthConfigRepository.findByService(serviceName).map { e =>
       Ok(Json.toJson(e))
     }
