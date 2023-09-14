082b4a41dee17fb3e61155bc3a565c4823deffaa
peteanning <22272449+peteanning@users.noreply.github.com>
2023-09-14 11:51:46 +0100

    BDOG-2535: merge main

--- test/uk/gov/hmrc/serviceconfigs/service/BobbyWarningsNotifierServiceSpec.scala
+++ test/uk/gov/hmrc/serviceconfigs/service/BobbyWarningsNotifierServiceSpec.scala
@@ -25,7 +25,7 @@ import play.api.Configuration
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.serviceconfigs.connector.{ServiceDependenciesConnector, SlackNotificationRequest, SlackNotificationResponse, SlackNotificationsConnector}
 import uk.gov.hmrc.serviceconfigs.model.{BobbyRule, BobbyRules}
-import uk.gov.hmrc.serviceconfigs.persistence.BobbyWarningsNotificationsRepository
+import uk.gov.hmrc.serviceconfigs.persistence.BobbyWarningsRepository
 
 import java.time.temporal.ChronoUnit.{DAYS, MONTHS, WEEKS}
 import java.time.temporal.TemporalAmount
@@ -43,46 +43,18 @@ class BobbyWarningsNotifierServiceSpec
 
 
   "The BobbyWarningsNotifierService" should {
-    "do nothing if the service has already been run in the notifications period" in new Setup {
-      when(mockBobbyWarningsRepository.getLastWarningsDate()).thenReturn(Future.successful(Some(yesterday)))
+    "do nothing if the service has already been run in the notifications period " in new Setup {
+      when(mockBobbyWarningsRepository.getLastWarningsDate()).thenReturn(Future.successful(yesterday))
 
       underTest.sendNotificationsForFutureDatedBobbyViolations.futureValue
 
       verifyZeroInteractions(mockBobbyRulesService, mockServiceDependenciesConnector, mockSlackNotificationsConnector)
-    }
-    "do nothing if the service has already been run today" in new Setup {
-      when(mockBobbyWarningsRepository.getLastWarningsDate()).thenReturn(Future.successful(Some(now)))
-
-      underTest.sendNotificationsForFutureDatedBobbyViolations.futureValue
-
-      verifyZeroInteractions(mockBobbyRulesService, mockServiceDependenciesConnector, mockSlackNotificationsConnector)
-    }
-
-
-    "run for the first time" in new Setup {
-
-      when(mockConfiguration.getOptional[String]("bobby-warnings-notifier-service.test-team")).thenReturn(None)
-
-      when(mockBobbyWarningsRepository.getLastWarningsDate()).thenReturn(Future.successful(None))
-      when(mockBobbyRulesService.findAllRules()).thenReturn(Future.successful(bobbyRules))
-
-
-      when(mockServiceDependenciesConnector.getDependentTeams(organisation, "exampleLib", range)).thenReturn(Future.successful(Seq(team1, team2)))
-      when(mockServiceDependenciesConnector.getDependentTeams(organisation, "exampleLib2", range)).thenReturn(Future.successful(Seq(team1)))
-
-      when(mockSlackNotificationsConnector.sendMessage(any[SlackNotificationRequest])(any[HeaderCarrier])).thenReturn(Future.successful(SlackNotificationResponse(Seq.empty)))
-
-      when(mockBobbyWarningsRepository.setLastRunDate(now)).thenReturn(Future.unit)
-
-      underTest.sendNotificationsForFutureDatedBobbyViolations.futureValue
-
-      verify(mockSlackNotificationsConnector, times(2)).sendMessage(any[SlackNotificationRequest])(any[HeaderCarrier])
     }
     "be run if the last time the service was run before the notifications period" in new Setup {
 
       when(mockConfiguration.getOptional[String]("bobby-warnings-notifier-service.test-team")).thenReturn(None)
 
-      when(mockBobbyWarningsRepository.getLastWarningsDate()).thenReturn(Future.successful(Some(eightDays)))
+      when(mockBobbyWarningsRepository.getLastWarningsDate()).thenReturn(Future.successful(eightDays))
       when(mockBobbyRulesService.findAllRules()).thenReturn(Future.successful(bobbyRules))
 
 
@@ -91,7 +63,7 @@ class BobbyWarningsNotifierServiceSpec
 
       when(mockSlackNotificationsConnector.sendMessage(any[SlackNotificationRequest])(any[HeaderCarrier])).thenReturn(Future.successful(SlackNotificationResponse(Seq.empty)))
 
-      when(mockBobbyWarningsRepository.setLastRunDate(now)).thenReturn(Future.unit)
+      when(mockBobbyWarningsRepository.updateLastWarningDate()).thenReturn(Future.unit)
 
       underTest.sendNotificationsForFutureDatedBobbyViolations.futureValue
 
@@ -105,24 +77,25 @@ class BobbyWarningsNotifierServiceSpec
 trait Setup  {
 
   val hc = HeaderCarrier()
+
   val team1 = "Team1"
   val team2 = "Team2"
+
+
   val organisation = "uk.gov.hmrc"
   val range = "[0.0.0,)"
-  val now: LocalDate = LocalDate.now()
-
-  val futureDatedBobbyRule1Week: BobbyRule = BobbyRule(organisation = organisation, name = "exampleLib", range =range, reason = "Bad Practice", from = now.plus(1L, WEEKS) , exemptProjects = Seq.empty)
-  val secondFutureDatedBobbyRule1Week: BobbyRule = BobbyRule(organisation = organisation, name = "exampleLib2", range =range, reason = "Bad Practice", from = now.plus(1L, WEEKS) , exemptProjects = Seq.empty)
-  val futureDatedRule3Months: BobbyRule = BobbyRule(organisation = organisation, name = "anotherExampleLib", range = range, reason = "Bad Practice", from = now.plus(3L, MONTHS) , exemptProjects = Seq.empty)
+  val futureDatedBobbyRule1Week: BobbyRule = BobbyRule(organisation = organisation, name = "exampleLib", range =range, reason = "Bad Practice", from = LocalDate.now().plus(1L, WEEKS) , exemptProjects = Seq.empty)
+  val secondFutureDatedBobbyRule1Week: BobbyRule = BobbyRule(organisation = organisation, name = "exampleLib2", range =range, reason = "Bad Practice", from = LocalDate.now().plus(1L, WEEKS) , exemptProjects = Seq.empty)
+  val futureDatedRule3Months: BobbyRule = BobbyRule(organisation = organisation, name = "anotherExampleLib", range = range, reason = "Bad Practice", from = LocalDate.now().plus(3L, MONTHS) , exemptProjects = Seq.empty)
 
   val bobbyRules = BobbyRules(libraries = Seq(futureDatedBobbyRule1Week, secondFutureDatedBobbyRule1Week, futureDatedRule3Months), plugins = Seq.empty)
 
-  val eightDays = now.minus(8L, DAYS)
-  val yesterday = now.minus(1L, DAYS)
+  val eightDays = LocalDate.now().minus(8L, DAYS)
+  val yesterday = LocalDate.now().minus(1L, DAYS)
 
   val mockBobbyRulesService = mock[BobbyRulesService]
   val mockServiceDependenciesConnector = mock[ServiceDependenciesConnector]
-  val mockBobbyWarningsRepository = mock[BobbyWarningsNotificationsRepository]
+  val mockBobbyWarningsRepository = mock[BobbyWarningsRepository]
   val mockSlackNotificationsConnector = mock[SlackNotificationsConnector]
 
 
